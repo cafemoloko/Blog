40abdccbadd383bedee25ee6093ad13914636eee
Karolina <karolina_piekarz@zut.edu.pl>
2019-09-03 22:02:48 +0200

    added custom error pages

--- blog/__init__.py
+++ blog/__init__.py
@@ -26,8 +26,11 @@ def create_app(config_class=Config):
     from blog.main.routes import main
     from blog.posts.routes import posts
     from blog.users.routes import users
+    from blog.errors.handlers import errors
+
     app.register_blueprint(main)
     app.register_blueprint(posts)
     app.register_blueprint(users)
+    app.register_blueprint(errors)
 
     return app
